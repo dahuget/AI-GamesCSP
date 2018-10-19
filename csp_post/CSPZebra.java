4197e2e52347e5b5c3c621f21fd57f19817ce283
dahuget <danaliyahuget@gmail.com>
2018-10-18 23:33:59 -0700

    first draft for question 3

---
+++ csp_post/CSPZebra.java
@@ -0,0 +1,113 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CSPZebra extends CSP {
+    static Set<Object> varCol = new HashSet<>(
+            Arrays.asList("blue", "green", "ivory", "red", "yellow"));
+    static Set<Object> varDri = new HashSet<>(
+            Arrays.asList("coffee", "milk", "orange-juice", "tea", "water"));
+    static Set<Object> varNat = new HashSet<>(
+            Arrays.asList("englishman", "japanese", "norwegian", "spaniard", "ukrainian"));
+    static Set<Object> varPet = new HashSet<>(
+            Arrays.asList("dog", "fox", "horse", "snails", "zebra"));
+    static Set<Object> varCig = new HashSet<>(
+            Arrays.asList("chesterfield", "kools", "lucky-strike", "old-gold", "parliament"));
+
+    public boolean isGood(Object X, Object Y, Object x, Object y) {
+        //if X is not even mentioned in by the constraints, just return true
+        //as nothing can be violated
+        if(!C.containsKey(X))
+            return true;
+
+        //check to see if there is an arc between X and Y
+        //if there isn't an arc, then no constraint, i.e. it is good
+        if(!C.get(X).contains(Y))
+            return true;
+
+        //The Englishman lives in the red house.
+        if(X.equals("englishman") && Y.equals("red") && !x.equals(y))
+            return false;
+
+        // … TODO
+
+        //Uniqueness constraints
+        if(varCol.contains(X) && varCol.contains(Y) && !X.equals(Y) && x.equals(y))
+            return false;
+
+        //… TODO
+
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        CSPZebra csp = new CSPZebra();
+        Integer[] dom = {1,2,3,4,5};
+        for(Object X : varCol)
+            csp.addDomain(X, dom);
+        for(Object X : varDri)
+            csp.addDomain(X, dom);
+        for(Object X : varNat)
+            csp.addDomain(X, dom);
+        for(Object X : varPet)
+            csp.addDomain(X, dom);
+        for(Object X : varCig)
+            csp.addDomain(X, dom);
+
+        //unary constraints: just remove values from domains
+        //8.Milk is drunk in the middle house.
+        csp.D.remove("milk", 3); //dom[2] ??
+        //9.The Norwegian lives in the first house on the left.
+        csp.D.remove("norwegian", 1); //dom[1] ??
+        //14.The Norwegian lives next to the blue house.
+        csp.D.remove("blue", 2);
+
+        //binary constraints: add constraint arcs
+        //1.The Englishman lives in the red house.
+        csp.addBidirectionalArc("englishman", "red");
+        //2.The Spaniard owns a dog.
+        csp.addBidirectionalArc("spaniard", "dog");
+        //3.Coffee is drunk in the green house.
+        csp.addBidirectionalArc("coffee", "green");
+        //4.The Ukrainian drinks tea.
+        csp.addBidirectionalArc("ukrainian", "tea");
+        //5.The green house is directly to the right of the ivory house.
+        // TODO (|green-ivory| =1)
+        csp.addBidirectionalArc("", "");
+        //6.The Old-Gold smoker owns snails.
+        csp.addBidirectionalArc("old-gold", "snails");
+        //7.Kools are being smoked in the yellow house.
+        csp.addBidirectionalArc("kools", "yellow");
+        //10.The Chesterfield smoker lives next to the fox owner.
+        // TODO (|chesterfield-fox| =1)
+        csp.addBidirectionalArc("", "");
+        //11.Kools are smoked in the house next to the house where the horse is kept.
+        // TODO (|kools-horse| =1)
+        csp.addBidirectionalArc("", "");
+        //12.The Lucky-Strike smoker drinks orange juice.
+        csp.addBidirectionalArc("lucky-strike", "orange-juice");
+        //13.The Japanese smokes Parliament.
+        csp.addBidirectionalArc("japanese", "parliament");
+
+        //Uniqueness constraints
+        for(Object X : varCol)
+            for(Object Y : varCol)
+                csp.addBidirectionalArc(X,Y);
+        for(Object X : varDri)
+            for(Object Y : varDri)
+                csp.addBidirectionalArc(X,Y);
+        for(Object X : varNat)
+            for(Object Y : varNat)
+                csp.addBidirectionalArc(X,Y);
+        for(Object X : varPet)
+            for(Object Y : varPet)
+                csp.addBidirectionalArc(X,Y);
+        for(Object X : varCig)
+            for(Object Y : varCig)
+                csp.addBidirectionalArc(X,Y);
+
+        //Now let's search for solution
+        Search search = new Search(csp);
+        System.out.println(search.BacktrackingSearch());
+    }
+}
