0cb69296a60bd869cf357543bda7c9020f167a12
dahuget <danaliyahuget@gmail.com>
2018-10-22 11:49:10 -0700

    complete nim game implementation

--- games_post/GameNim.java
+++ games_post/GameNim.java
@@ -1,3 +1,9 @@
+/*
+ * Dana Huget, V00860786
+ * October 22, 2018
+ * CSC 421 A2 Question 1 Solution
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -96,20 +102,20 @@ public class GameNim extends Game {
                 case 1: //Human
 
                     //get human's move
-                    System.out.print("Enter your *valid* move> ");
+                    System.out.print("How many *valid* coins do you want to take > ");
                     int move = checkValidMove(in);
 
                     nextState = new StateNim((StateNim)game.currentState);
                     nextState.player = 1;
                     nextState.pile -= move;
-                    System.out.println("Human: \n" + nextState);
+                    System.out.printf("You took %d coins, the pile has %s left\n", move, nextState);
                     break;
 
                 case 0: //Computer
 
                     nextState = (StateNim)search.bestSuccessorState(depth);
                     nextState.player = 0;
-                    System.out.println("Computer: \n" + nextState);
+                    System.out.printf("Computer took some coins, the pile has %s left\n", nextState);
                     break;
             }
 
@@ -127,87 +133,6 @@ public class GameNim extends Game {
 
                 break;
             }
-
-            if ( game.isStuckState(game.currentState) ) {
-                System.out.println("Cat's game!");
-                break;
-            }
         }
     }
 }
-
-/*
-
-    public static void main(String[] args) throws Exception {
-
-        Game game = new GameTicTacToe();
-        Search search = new Search(game);
-        int depth = 8;
-
-        //needed to get human's move
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-        while (true) {
-
-        	StateTicTacToe 	nextState = null;
-
-            switch ( game.currentState.player ) {
-              case 1: //Human
-
-            	  //get human's move
-                  System.out.print("Enter your *valid* move> ");
-                  int pos = Integer.parseInt( in.readLine() );
-
-                  nextState = new StateTicTacToe((StateTicTacToe)game.currentState);
-                  nextState.player = 1;
-                  nextState.board[pos] = 'X';
-                  System.out.println("Human: \n" + nextState);
-                  break;
-
-              case 0: //Computer
-
-            	  nextState = (StateTicTacToe)search.bestSuccessorState(depth);
-            	  nextState.player = 0;
-            	  System.out.println("Computer: \n" + nextState);
-                  break;
-            }
-
-            game.currentState = nextState;
-            //change player
-            game.currentState.player = (game.currentState.player==0 ? 1 : 0);
-
-            //Who wins?
-            if ( game.isWinState(game.currentState) ) {
-
-            	if (game.currentState.player == 1) //i.e. last move was by the computer
-            		System.out.println("Computer wins!");
-            	else
-            		System.out.println("You win!");
-
-            	break;
-            }
-
-            if ( game.isStuckState(game.currentState) ) {
-            	System.out.println("Cat's game!");
-            	break;
-            }
-        }
-    }
-}
- */
-/**gets the computer's move, 1 or 2.  Currently
- * this method gets a random number.  Your job
- * is to make the computer choose such that it
- * wins every time it is possible.  First, solve
- * with recursion.  After successfully completing
- * this, run the program a few times to see if you
- * can recognize the pattern the computer is taking.
- * Then see if you can get the computer to choose
- * its move without looping or recursion.
- * @param left
- * @return
- * public int getComputerMove(int left)
- *         {
- *         return (int)(Math.random()*2)+1;
- *         }
- */
--- games_post/StateNim.java
+++ games_post/StateNim.java
@@ -1,7 +1,13 @@
 /*
- This particular version of Nim has a pile of
- 13 coins on the table, starts with human as player 1
+ * Dana Huget, V00860786
+ * October 22, 2018
+ * CSC 421 A2 Question 1 Solution
+ *
+ * This particular version of Nim has a pile of
+ * 13 coins on the table, starts with human as player 1
+ *
  */
+
 public class StateNim extends State {
     public int pile;
 
